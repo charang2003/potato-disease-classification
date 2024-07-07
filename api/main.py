b200d1bd43141835796d90577d695d312cb76c68
unknown <charang2003@gmail.com>
2024-07-07 19:44:51 +0530

    added urls and read image function

--- api/main.py
+++ api/main.py
@@ -1,4 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import uvicorn
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
 
 app = FastAPI()
 
+@app.get("/ping")
+async def ping():
+    return "hello"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    bytes = await file.read()
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
